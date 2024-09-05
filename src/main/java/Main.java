28a133afac2c34c43df6180132f210637a461cb8
zyzz2011 <169068710+zyzz2011@users.noreply.github.com>
2024-09-05 10:47:06 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("programa que soma dois números");
+  system.out.println("o primeiro número é:");
+    
+  int numero2 = 10;
+
+
+
+}
+
+
+
+
